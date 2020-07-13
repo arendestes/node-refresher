64741560d35f8cb4b07786ea733035c4627a7f66
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-07-13 12:20:57 -0500

    used node to transfer data and handle http requests and responses without express

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const http = require('http');
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     console.log('INCOMING REQUEST');
     console.log(req.method, req.url);
 
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Response</h1>');
+    if (req.method === 'POST') {
+        let body = '';
+        req.on('end', ()=>{
+            console.log(body);
+            let userName = body.split('=')[1];
+            console.log(userName);
+            res.end(`<h1>${userName}</h1>`);
+        })
+        req.on("data", chunk => {
+            body += chunk;
+        })
+       
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<form method="POST"><input type="text" name="userName"></input><button type="submit">Create User</button></form>');
+    }
 });
 
 server.listen(5000);
